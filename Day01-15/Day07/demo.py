80dd44d9ceef8ae44814698424e1569624547716
谭小波 <tanxiaobo@quanyou.com.cn>
2020-12-17 23:19:49 +0800

    11

---
+++ Day01-15/Day07/demo.py
@@ -0,0 +1,20 @@
+# str1='a1b2c3d4e5'
+# print(str1[1])
+# print(str1[2:5])
+# print(str1[2:])
+# print(str1.isdigit())
+# print(str1.isalpha())
+# print(str1.isalnum())
+# print(str1[-3:-1])
+# print(str1[::-1])
+
+
+
+
+
+# 列表
+
+list1=[1,3,5,7,100]
+print(list1)
+list2=['fuck']*5
+print(list2)
